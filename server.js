d6c65e40d078103e7611f2be383b16b72db81c42
Glitch (fedifinder) <none>
2022-10-31 13:17:46 +0000

    show handles of the logged in account

--- server.js
+++ server.js
@@ -178,7 +178,10 @@ app.get(
             "Cache-Control",
             "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
           );
-          res.render("success.hbs", { handles });
+          res.render("success.hbs", {
+            handles: handles,
+            profile: findHandles(user_to_text(req.user._json)),
+          });
         }
       }
     );
