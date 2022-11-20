e154db535adc6507948b3926e3c499a12ce92844
Glitch (fedifinder-backup2) <none>
2022-11-20 12:48:38 +0000

    try catch

--- server.js
+++ server.js
@@ -204,7 +204,6 @@ app.get(
   }
 );
 
-
 /*app.get("/", (req, res) => {
   "code" in req.query
     ? res.redirect("/index.html#c=" + req.query.code)
@@ -804,33 +803,39 @@ app.get("/api/lookupServer", async (req, res) => {
 });
 
 app.get("/api/loadLists", async (req, res) => {
-  let client = create_twitter_client(req.user);
-  let lists = [];
+  try {
+    let client = create_twitter_client(req.user);
+    let lists = [];
 
-  // get lists owned by user
-  const ownedLists = await client.v2.listsOwned(req.user.id, {
-    "list.fields": ["member_count"],
-  });
-  for await (const list of ownedLists) {
-    lists.push({
-      name: list["name"],
-      id_str: list["id"],
-      member_count: list["member_count"],
+    // get lists owned by user
+    const ownedLists = await client.v2.listsOwned(req.user.id, {
+      "list.fields": ["member_count"],
     });
-  }
+    for await (const list of ownedLists) {
+      lists.push({
+        name: list["name"],
+        id_str: list["id"],
+        member_count: list["member_count"],
+      });
+    }
 
-  // get subscribed lists of user
-  const followedLists = await client.v2.listFollowed(req.user.id, {
-    "list.fields": ["member_count"],
-  });
-  for await (const list of followedLists) {
-    lists.push({
-      name: list["name"],
-      id_str: list["id"],
-      member_count: list["member_count"],
+    // get subscribed lists of user
+    const followedLists = await client.v2.listFollowed(req.user.id, {
+      "list.fields": ["member_count"],
     });
+    for await (const list of followedLists) {
+      lists.push({
+        name: list["name"],
+        id_str: list["id"],
+        member_count: list["member_count"],
+      });
+    }
+    res.json(lists);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+    return;
   }
-  res.json(lists);
 });
 
 app.get("/api/getList", async (req, res) => {
