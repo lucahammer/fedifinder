8979a3b8750ee2cad0d52d35fe79939b932caf18
Glitch (fedifinder-staging) <none>
2022-12-01 19:48:05 +0000

    don't check all followings in staging

--- public/client.js
+++ public/client.js
@@ -440,7 +440,9 @@ const app = Vue.createApp({
         this.known_instances = await cached_data.json();
         this.twitter_auth = true;
         this.loadProfile();
-        this.loadFollowings();
+        if (!window.location.hostname.includes("staging")) {
+          this.loadFollowings();
+        }
         this.loadLists();
       } catch (err) {
         this.logoff();
